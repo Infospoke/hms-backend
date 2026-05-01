2f108923605bef0290b40247ff0ba11ab783ca50
athiyainfospoke <athiyainfospoke@gmail.com>
2026-05-01 18:32:10 +0530

    added  kissed repository

---
+++ src/main/java/com/hms/service/repository/InterviewSessionRepository.java
@@ -0,0 +1,19 @@
+package com.hms.service.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.hms.service.entity.InterviewSessionEntity;
+
+@Repository
+public interface InterviewSessionRepository extends JpaRepository<InterviewSessionEntity, Integer> {
+
+
+	@Query("SELECT i.applicationId, i.status FROM InterviewSessionEntity i WHERE i.applicationId IN :applicationIds AND (i.isDeleted = false OR i.isDeleted IS NULL)")
+			List<Object[]> findApplicationIdAndStatus(@Param("applicationIds") List<Integer> applicationIds);
+	
+}
