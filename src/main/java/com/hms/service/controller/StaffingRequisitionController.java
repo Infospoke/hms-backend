fc6cb10b4e80d7228e495038104c3d490c106a18
rithikainfospoke <rithikainfospoke@gmail.com>
2026-05-19 14:33:04 +0530

    removed unused imports

--- src/main/java/com/hms/service/controller/StaffingRequisitionController.java
+++ src/main/java/com/hms/service/controller/StaffingRequisitionController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.hms.service.request.SRFilterRequest;
 import com.hms.service.request.SpecificationFilterRequest;
 import com.hms.service.request.StaffingRequisitionRequest;
 import com.hms.service.request.UpdateSrRequest;
--- src/main/java/com/hms/service/repository/FunctionalityRepository.java
+++ src/main/java/com/hms/service/repository/FunctionalityRepository.java
@@ -4,15 +4,10 @@ package com.hms.service.repository;
 import java.util.Optional;
 
 import java.util.List;
-import java.util.Optional;
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
-
 import com.hms.service.entity.FunctionalityEntity;
-import com.hms.service.entity.SRPositionBasicsEntity;
+
 
 
 @Repository
