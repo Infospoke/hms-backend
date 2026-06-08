b3312e3076a93d5b46ec1601edc1cb7ee4889c61
rithikainfospoke <rithikainfospoke@gmail.com>
2026-06-08 17:42:25 +0530

    removed unused repo

--- src/main/java/com/hms/service/repository/InterviewCandidateDetailsRepository.java
+++ src/main/java/com/hms/service/repository/InterviewCandidateDetailsRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import com.hms.service.entity.InterviewCandidateDetailsEntity;
 
+@Repository
 public interface InterviewCandidateDetailsRepository extends JpaRepository<InterviewCandidateDetailsEntity, Integer>,JpaSpecificationExecutor<InterviewCandidateDetailsEntity>{
 
 	long countByUserIdAndCreatedOnBetween(Integer userId, LocalDateTime startDate, LocalDateTime endDate);
