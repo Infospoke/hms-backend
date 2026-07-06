708743eb5df205f8f8bbb0a265c26a5ab67ead2a
alavalapatialekhya15 <alavalapatialekhya15@gmail.com>
2026-07-06 16:58:24 +0530

    InterviewAssignmentCount api added

--- src/main/java/com/hms/service/service/IInterviewerAssignmentService.java
+++ src/main/java/com/hms/service/service/IInterviewerAssignmentService.java
@@ -22,5 +22,7 @@ public interface IInterviewerAssignmentService {
 
 	ApiResponse<?> updateInterviewAssignment(UpdateInterviewAssignmentRequest request);
 
+	ApiResponse<?> getInterviewersAssignmentCounts();
+
 	
 }
