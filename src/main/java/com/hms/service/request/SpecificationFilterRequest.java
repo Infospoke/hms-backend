0623ed699c78e51f1d52f7cd95982e3149ba79f5
alavalapatialekhya15 <alavalapatialekhya15@gmail.com>
2026-07-08 14:15:25 +0530

    changes done in SpecificationFilterRequest

--- src/main/java/com/hms/service/request/SpecificationFilterRequest.java
+++ src/main/java/com/hms/service/request/SpecificationFilterRequest.java
@@ -1139,6 +1139,7 @@ public class SpecificationFilterRequest {
 			// Logged-in user filter
 			predicates.add(cb.equal(root.get("interviewerId"), userId));
 			predicates.add(cb.equal(root.get("interviewDate"), LocalDate.now()));
+			predicates.add(cb.isFalse(root.get("interviewCompleted")));
   		String interviewType = getFilter("interviewType");
 
 			if (interviewType != null && !interviewType.isBlank()) {
