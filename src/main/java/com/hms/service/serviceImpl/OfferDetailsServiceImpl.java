60330d539e6cd465a031947b6d073a7f5cf9c2b9
Rithika <rithikainfospoke@gmail.com>
2026-07-10 17:12:28 +0530

    removed finalapprovaltime and added date of approval 3 for filters&sla in offer release list

--- src/main/java/com/hms/service/serviceImpl/OfferDetailsServiceImpl.java
+++ src/main/java/com/hms/service/serviceImpl/OfferDetailsServiceImpl.java
@@ -131,7 +131,7 @@ public class OfferDetailsServiceImpl implements IOfferDetailsService {
 
 						? request.getSortBy()
 
-						: "finalApprovalTime"
+						: "dateOfApproval3"
 
 		);
 
@@ -191,9 +191,9 @@ public class OfferDetailsServiceImpl implements IOfferDetailsService {
 
 		map.put("recruiterName", offer.getRecruitedBy());
 
-		map.put("finalApprovalTime", offer.getFinalApprovalTime());
+		map.put("finalApprovalTime", offer.getDateOfApproval3());
 
-		map.put("priority", calculatePriority(offer.getFinalApprovalTime()));
+		map.put("priority", calculatePriority(offer.getDateOfApproval3()));
 
 		map.put("totalCtc", offer.getTotalCtc());
 
@@ -201,9 +201,9 @@ public class OfferDetailsServiceImpl implements IOfferDetailsService {
 
 	}
 
-	private String calculatePriority(LocalDateTime finalApprovalTime) {
+	private String calculatePriority(LocalDateTime dateOfApproval3) {
 
-		long days = ChronoUnit.DAYS.between(finalApprovalTime.toLocalDate(), LocalDate.now());
+		long days = ChronoUnit.DAYS.between(dateOfApproval3.toLocalDate(), LocalDate.now());
 
 		if (days >= 5) {
 			return "High";
