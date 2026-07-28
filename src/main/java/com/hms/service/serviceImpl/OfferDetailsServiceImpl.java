befd65febc14a0bc5031c1ab4ff2898685f53236
tharunyainfospoke <tharunyainfospoke@gmail.com>
2026-07-28 11:41:37 +0530

    joining date in candidate offer details

--- src/main/java/com/hms/service/serviceImpl/OfferDetailsServiceImpl.java
+++ src/main/java/com/hms/service/serviceImpl/OfferDetailsServiceImpl.java
@@ -294,7 +294,7 @@ public class OfferDetailsServiceImpl implements IOfferDetailsService {
 						Boolean.TRUE.equals(budget.getSigningBonus()) ? budget.getSigningBonusAmount() : 0);
 
 				response.setAnnualRsuEsopValue(Boolean.TRUE.equals(budget.getEquity()) ? budget.getEquityAmount() : 0);
-
+				
 				response.setOtherBenefits(
 						Boolean.TRUE.equals(budget.getRelocationBudget()) ? budget.getRelocationBudgetAmount() : 0);
 
@@ -319,6 +319,7 @@ public class OfferDetailsServiceImpl implements IOfferDetailsService {
 
 				response.setProbationPeriod(offer.getProbationPeriod());
 				response.setNoticePeriod(offer.getNoticePeriod());
+				response.setJoiningDate(offer.getJoiningDate());				
 
 			}
 			log.info("OfferDetailsServiceImpl ::Exit from the getOfferDetailsByApplicantId");
