19e1d9886c20263a194cd13d208fdf1ca3ab66d3
Geethu0912 <ayyapaneni.g@infospoke.in>
2026-08-17 16:00:12 +0530

    progress count changes

--- src/main/java/com/hms/service/repository/JobApplicationRepository.java
+++ src/main/java/com/hms/service/repository/JobApplicationRepository.java
@@ -26,39 +26,6 @@ public interface JobApplicationRepository extends JpaRepository<JobApplicationEn
 
 	Optional<JobApplicationEntity> findById(Integer id);
 
-	@Query(value = """
-			SELECT
-			    (SELECT COUNT(*)
-			     FROM tb_job_applications
-			     WHERE in_person_interviews = TRUE),
-
-			    (SELECT COUNT(DISTINCT application_id)
-			     FROM tb_interview_sessions
-			     WHERE UPPER(status) = 'COMPLETED'),
-
-			    (SELECT COUNT(DISTINCT cs.application_id)
-			     FROM tb_interview_current_stage cs
-			     INNER JOIN tb_interview_round_dropdown rd
-			            ON cs.current_stage_type = rd.id
-			     WHERE cs.interview_completed = TRUE
-			       AND LOWER(rd.round_name) = 'techincal round'),
-
-			    (SELECT COUNT(DISTINCT cs.application_id)
-			     FROM tb_interview_current_stage cs
-			     INNER JOIN tb_interview_round_dropdown rd
-			            ON cs.current_stage_type = rd.id
-			     WHERE cs.interview_completed = TRUE
-			       AND LOWER(rd.round_name) = 'mangerial round'),
-
-			    (SELECT COUNT(DISTINCT cs.application_id)
-			     FROM tb_interview_current_stage cs
-			     INNER JOIN tb_interview_round_dropdown rd
-			            ON cs.current_stage_type = rd.id
-			     WHERE cs.interview_completed = TRUE
-			       AND LOWER(rd.round_name) = 'hr round')
-			""", nativeQuery = true)
-	List<Object[]> getInterviewDashboard();
-
 	@Query("""
 			 SELECT j.jobId, COUNT(j)
 			 FROM JobApplicationEntity j
@@ -123,6 +90,8 @@ public interface JobApplicationRepository extends JpaRepository<JobApplicationEn
 	List<JobApplicationEntity> findApplicationsByRecruiterAndJob(@Param("recruiterId") Integer recruiterId,
 			@Param("jobId") Integer jobId, @Param("fromDate") LocalDateTime fromDate,
 			@Param("toDate") LocalDateTime toDate);
+
+	long countByInPersonInterviewsTrue();
 
 
 }
